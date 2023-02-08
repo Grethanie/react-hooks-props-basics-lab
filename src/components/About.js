a71aa162fa12f2ac905bd9df54f34d66b47b0973
Daniel Young <Danyoung91@gmail.com>
2023-02-08 14:48:20 -0500

    commit tree 3655b085970aae9bebfd1ae27310592fe0c01f1e

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,17 @@
 import React from "react";
 
-function About() {
+function About(props) {
+  const isBio = () => {
+    if (props.bio) {
+      return <p>{props.bio}</p>;
+    }
+  };
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {isBio()}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin} />
     </div>
   );
 }
